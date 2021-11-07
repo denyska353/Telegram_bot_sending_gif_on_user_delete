685438f2994fa34b76709c67939851f6731ca137
denyska353 <denys.ivamyshyn@gmail.com>
2021-11-07 18:36:33 +0200

    Project start //spring+pdf_adding+del_member

--- src/main/java/Denys_bot/BOT_Telgramm_01/service/MateAcademyBot.java
+++ src/main/java/Denys_bot/BOT_Telgramm_01/service/MateAcademyBot.java
@@ -37,57 +37,5 @@ public class MateAcademyBot extends TelegramLongPollingBot {
             System.out.println(message.getLeftChatMember());
             System.out.println("Delete message geted");
         }
-        String filePath = "D:\\java\\File\\qwer.pdf";
-        String filePath2 = "D:\\java\\File\\qwer1.pdf";
-        File file = new File(filePath);
-        File file2 = new File(filePath2);
-
-        if (!file.exists())
-            System.out.println("no such file");
-        try {
-            // inout stream from file
-            InputStream inputStream = new FileInputStream(file);
-
-            // we create a reader for a certain document
-            PdfReader reader = new PdfReader(inputStream);
-
-
-            // we create a stamper that will copy the document to a new file
-            PdfStamper stamp = new PdfStamper(reader, new FileOutputStream(file2));
-
-            // adding content to each page
-            int i = 0;
-            PdfContentByte under;
-            under = stamp.getOverContent(1);
-            under.beginText();
-            under.setFontAndSize(BaseFont.createFont(),15);
-            under.setTextMatrix(20,600);
-            under.showText(message.getText());
-
-            under.endText();
-            stamp.close();
-
-        }
-        catch(Exception de) {
-            de.printStackTrace();
-        }
-        InputFile inputStream2 = new InputFile(file2);
-        System.out.println(message.getText());
-        SendDocument sendDocumentRequest = new SendDocument();
-        sendDocumentRequest.setChatId(message.getChatId().toString());
-        sendDocumentRequest.setDocument(inputStream2);
-
-        //sendDocumentRequest.setThumb(inputStream2);
-
-
-        //sendDocumentRequest.setCaption(caption);
-        //SendMessage sendMessage = new SendMessage();
-        //sendMessage.setText("Hello " + message.getText());
-        //sendMessage.setChatId(message.getChatId().toString());
-        try {
-            execute(sendDocumentRequest);
-        } catch (TelegramApiException e) {
-            e.printStackTrace();
-        }
     }
 }
